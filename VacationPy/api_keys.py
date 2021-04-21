6e9fe99c32498d58ed76eb502339f84c8e00af73
foofx <fangxuan.foo@gmail.com>
2021-04-21 20:45:16 +0800

    Completed Codes with Observations

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "9a5e2464ed3bbfb6f5291e7d8c813760"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCwYKjpGFNhF_5l0qAND2eaPGmxGworERI"
